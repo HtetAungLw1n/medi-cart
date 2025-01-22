83f851f14c231a613c19fc303d3ad9898362a92a
Htet Aung Lwin <htetaunglwin.c@non@gmail.com>
2025-01-22 23:07:32 +0630

    add font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00897B",
+        secondary: "#43A047",
+        accent: "#BBDEFB",
+        background: "#F5F5F5",
+        textcolor: "#212121",
+      },
+      fontFamily: {
+        lexend: ["Lexend"],
+      },
+    },
   },
   plugins: [],
 };
